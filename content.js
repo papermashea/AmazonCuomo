c89facd64c3de8db901a4ae08c29c1896cc7d716
papermashea <molloy.shea@gmail.com>
2018-11-20 10:57:20 -0500

    adding logos and scripts for full name

--- content.js
+++ content.js
@@ -16,3 +16,41 @@ for (var i = 0; i < elements.length; i++) {
         }
     }
 }
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/Andrew Mark Cuomo/gi, 'Amazon Cuomo');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/Andrew M. Cuomo/gi, 'Amazon Cuomo');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
